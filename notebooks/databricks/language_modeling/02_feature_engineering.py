34e9d8311c0f1d25991421d8cb2cd6c979aa58e7
rafael.pierre@databricks.com <rafael.pierre@databricks.com>
2022-08-10 15:13:02 +0000

    context

---
+++ notebooks/databricks/language_modeling/02_feature_engineering.py
@@ -0,0 +1,111 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC 
+# MAGIC ## Feature Engineering
+# MAGIC <br/>
+# MAGIC 
+# MAGIC * In this notebook, we perform **feature engineering** over the dialogue data that we downloaded previously.
+# MAGIC * Multi-turn conversational models require multiple columns for each of the parts of the **context**.
+# MAGIC * The **context** in this case represents the different **utterances** that are part of the conversation.
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC 
+# MAGIC select * from persuasiondb.full_dialog order by conversation_id, id asc
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ### High Level Analysis
+# MAGIC 
+# MAGIC Let's look at how our dialogues are distributed in terms of number of **turns/utterances**.
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC 
+# MAGIC select count(1) as num_turns
+# MAGIC from persuasiondb.full_dialog
+# MAGIC group by conversation_id
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC We have a long tail. Let's look into some basic summary statistics:
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC 
+# MAGIC select 
+# MAGIC   avg(num_turns),
+# MAGIC   std(num_turns),
+# MAGIC   max(num_turns),
+# MAGIC   min(num_turns)
+# MAGIC from (
+# MAGIC select count(1) as num_turns
+# MAGIC from persuasiondb.full_dialog
+# MAGIC group by conversation_id
+# MAGIC )
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC * Each dialogue has on average 20 turns, with a standard deviation of approximately one. Our dialogues are somewhat homogeneous in terms of length.
+# MAGIC * To train our model, we will have a label column which indicates the answers that we want our chatbot to give us.
+# MAGIC * We will apply a rolling window function, where the label will be the last conversation turn up until that record, and the other columns will be the previous turns.
+# MAGIC * For the sake of simplicity, let's work only with conversations which have 20 or less turns.
+
+# COMMAND ----------
+
+query = """select conversation_id from
+  (select
+     count(1) as num_turns,
+     conversation_id
+   from persuasiondb.full_dialog
+   group by conversation_id
+  )
+where num_turns <= 20"""
+
+df_conversations = spark.sql("select * from persuasiondb.full_dialog")
+df_filtered = spark.sql(query).join(df_conversations, ["conversation_id"])
+display(df_filtered)
+
+# COMMAND ----------
+
+df_filtered.write.saveAsTable("persuasiondb.filtered_conversations", mode="overwrite")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ## Building Conversation Contexts
+
+# COMMAND ----------
+
+from pyspark.sql.window import Window
+from pyspark.sql import functions as F
+
+df_context = df_filtered.withColumnRenamed("utterance", "label")
+window  = Window.partitionBy("conversation_id").orderBy(F.col("id").desc())
+context_length = 20
+
+for i in range(1, context_length + 1):
+  df_context = df_context.withColumn(f"context/{i}", F.lead(F.col("label"), i).over(window))
+  
+display(df_context.where("agent = 0").orderBy(F.col("conversation_id"), F.col("id").desc()))
+
+# COMMAND ----------
+
+df_context = df_context.where("agent = 0").orderBy(F.col("conversation_id"), F.col("id").desc())
+df_context.write.saveAsTable("persuasiondb.dialog_contextualized")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC 
+# MAGIC select count(1) from persuasiondb.dialog_contextualized
