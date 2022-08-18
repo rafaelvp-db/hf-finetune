fa4a7b5d8db3f0fcc9713450329338a1f0b907f5
rafael.pierre@databricks.com <rafael.pierre@databricks.com>
2022-08-18 19:56:47 +0000

    gradio
    gradio

--- notebooks/databricks/language_modeling/05_inference.py
+++ notebooks/databricks/language_modeling/05_inference.py
@@ -3,6 +3,12 @@
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC 
+# MAGIC ## Loading our Fine Tuned Model from MLflow
+
+# COMMAND ----------
+
 from transformers.pytorch_utils import *
 import mlflow
 from mlflow.tracking import MlflowClient
@@ -35,6 +41,12 @@ model.to("cpu")
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC 
+# MAGIC ## Generating our Chatbot Messages
+
+# COMMAND ----------
+
 import torch
 import numpy as np
 
@@ -50,6 +62,11 @@ def generate(
   no_repeat_ngram_size = 3,
   length_penalty = 20.0
 ):
+  """
+    This function is a wrapper on top of our fine tuned model.
+    It preprocesses and tokenizes our inputs, and calls the generate function
+    from the DialoGPT model we trained.
+  """
   
   new_user_input_ids = tokenizer.encode(
     str(question) + str(tokenizer.eos_token),
@@ -58,9 +75,11 @@ def generate(
 
   chat_history_tensor = torch.tensor(chat_history_ids)
   if (len(chat_history_ids) > 0):
+    # Beginning of conversation
     bot_input_ids = torch.cat([chat_history_tensor, torch.flatten(new_user_input_ids)], dim = -1)
     bot_input_ids = torch.reshape(bot_input_ids, shape = (1, -1))
   else:
+    # Continuing an existing conversation
     bot_input_ids = new_user_input_ids
     
   chat_history_ids = model.generate(
@@ -77,12 +96,10 @@ def generate(
     length_penalty = length_penalty,
     forced_eos_token_id = tokenizer.eos_token_id
   )
-  
-  """chat_history_ids = model.generate(
-    bot_input_ids,
-    eos_token_id = tokenizer.eos_token_id
-  )"""
 
+  # Using our tokenizer to decode the outputs from our model
+  # (e.g. convert model outputs to text)
+  
   answer = tokenizer.decode(
     chat_history_ids[:, bot_input_ids.shape[-1]:][0],
     skip_special_tokens = True
@@ -104,6 +121,12 @@ def predict(model_input):
   }
 
   return result
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ## Chatting with our Model
 
 # COMMAND ----------
 
@@ -152,3 +175,72 @@ chat_history_ids = result["chat_history_ids"]
 
 print("Question: ", model_input["question"])
 print("Answer: ", answer)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ## Creating an Interface with Gradio
+
+# COMMAND ----------
+
+!pip install gradio
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ### Step 1: The Basics
+
+# COMMAND ----------
+
+import gradio as gr
+
+flagging_dir = "/tmp/flagged"
+
+def greet(name):
+    return "Hello " + name + "!"
+
+demo = gr.Interface(fn = greet, inputs = "text", outputs = "text", flagging_dir = flagging_dir)
+routes = demo.launch(share = True)
+
+# COMMAND ----------
+
+displayHTML(f"<h3>We can access our Gradio by clicking this link: <a href='{routes[2]}'>Gradio App</a> 😀</h2>")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ## Step 2: Plugging our DialoGPT Model into our Gradio App
+# MAGIC 
+# MAGIC * Plugging our chatbot into our Gradio app is simple - it can be done with 3 lines of code!
+# MAGIC * However, we need to change our `predict` function slightly
+
+# COMMAND ----------
+
+def predict(text, history = []):
+    # tokenize the new input sentence
+    new_user_input_ids = tokenizer.encode(text + tokenizer.eos_token, return_tensors='pt')
+
+    # append the new user input tokens to the chat history
+    bot_input_ids = torch.cat([torch.LongTensor(history), new_user_input_ids], dim=-1)
+
+    # generate a response 
+    history = model.generate(bot_input_ids, max_length=30, pad_token_id=tokenizer.eos_token_id).tolist()
+
+    # convert the tokens to text, and then split the responses into lines
+    response = tokenizer.decode(history[0]).split("<|endoftext|>")
+    response = [(response[i], response[i+1]) for i in range(0, len(response)-1, 2)]  # convert to tuples of list
+    return response, history
+
+routes = gr.Interface(
+  fn = predict,
+  inputs = ["text", "state"],
+  outputs = ["chatbot", "state"],
+  allow_flagging = "never"
+).launch(share = True)
+
+# COMMAND ----------
+
+
